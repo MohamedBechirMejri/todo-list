051d8eb8ff603b99f1cc31686378e48541d58b70
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-03 09:18:05 +0100

    brb

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { NoteMaker } from "./js/NoteMaker";
 import { projects } from "./js/projects";
 import { generateProjectsList, generateNotes } from "./js/DOMStuff";
 
+
+/**
 let test = NoteMaker(
   "title",
   "description",
@@ -19,10 +21,7 @@ projects.addToProject("test3", test);
 
 generateProjectsList(projects);
 generateNotes(projects.test);
-/**
 
- *create a DOM manipulating function that adds project arrays to a sidebar
- *create a function that generates notes from an array based on the selected project
  *save arrays in localStorage
  *on page load see if there are any arrays stored in localStorage and generate them
  */
