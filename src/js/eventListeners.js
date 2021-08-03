d0f0851edc91b8a1afa4d6fb1c0dbc1f42ee152d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-03 17:17:27 +0100

    make checkbox button functional

--- src/js/eventListeners.js
+++ src/js/eventListeners.js
@@ -1,43 +1,56 @@
-import { addNewNote } from "./DOMStuff";
-import { NoteMaker } from "./NoteMaker";
-import { projects } from "./projects";
+import {
+    addNewNote
+} from "./DOMStuff";
+import {
+    NoteMaker
+} from "./NoteMaker";
+import {
+    projects
+} from "./projects";
 
 export const listenToNewNoteButton = () => {
-  const newNoteButton = document.getElementById("add-note");
-  newNoteButton.addEventListener("click", addNewNote);
+    const newNoteButton = document.getElementById("add-note");
+    newNoteButton.addEventListener("click", addNewNote);
 };
 
 export const listenToSaveButton = (newNote) => {
-  const saveButton = document.getElementById("save-button");
-  saveButton.addEventListener("click", () => {
-    const title = document.getElementById("new-title");
-    const description = document.getElementById("new-description");
-    const checkList = document.querySelectorAll(".new-checkbox");
-    const project = document.getElementById("projects");
+    const saveButton = document.getElementById("save-button");
+    saveButton.addEventListener("click", () => {
+        const title = document.getElementById("new-title");
+        const description = document.getElementById("new-description");
+        const checkList = document.querySelectorAll(".new-checkbox");
+        const project = document.getElementById("projects");
 
-    let checkboxes = [];
-    checkList.forEach((checkbox) => checkboxes.push(checkbox.value));
+        let checkboxes = [];
+        checkList.forEach((checkbox) => checkboxes.push(checkbox.value));
 
-    projects.addToProject(
-      project.value,
-      NoteMaker(
-        title.value,
-        description ? description.value : "",
-        checkboxes,
-        "date"
-      )
-    );
-    newNote.innerHTML = "";
-  });
+        projects.addToProject(
+            project.value,
+            NoteMaker(
+                title.value,
+                description ? description.value : "",
+                checkboxes,
+                "date"
+            )
+        );
+        newNote.innerHTML = "";
+    });
 };
 export const listenToDescriptionButton = () => {
-  const descriptionButton = document.getElementById("description-button");
+    const descriptionButton = document.getElementById("description-button");
 
-  descriptionButton.addEventListener("click", () => {
-    document.getElementById(
-      "description-div"
-    ).innerHTML = `<textarea id="new-description" placeholder="Description"></textarea>`;
-  }); //TODO: toggle textarea instead of just showing it once
+    descriptionButton.addEventListener("click", () => {
+        document.getElementById(
+            "description-div"
+        ).innerHTML = `<textarea id="new-description" placeholder="Description"></textarea>`;
+    }); //TODO: toggle textarea instead of just showing it once
+};
+export const listenToCheckboxButton = () => {
+    const checkboxButton = document.getElementById("add-check-item-button");
+    checkboxButton.addEventListener("click", () => {
+        document.getElementById(
+            "checkboxes-div"
+        ).innerHTML += `<input class="new-checkbox" placeholder="Checklist Item"/>`;
+    });
 };
 // export const listenToScheduleButton = () => {};
-// export const listenToCheckboxButton = () => {};
