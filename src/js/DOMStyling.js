e12ec927f6b63d34da810e3ad368d03a311c9f7e
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-04 14:42:59 +0100

    fix: :bug: fix gap height not changing when set to 0

--- src/js/DOMStyling.js
+++ src/js/DOMStyling.js
@@ -3,8 +3,8 @@ export const DOMStyling = (() => {
 
     navToggle.addEventListener("click", () => {
         const gapDiv = document.getElementById("gap");
-        if (gapDiv.style.height !== 0) {
-            gapDiv.style.height = 0;
+        if (gapDiv.style.height !== '0em') {
+            gapDiv.style.height = '0em';
         } else {
             gapDiv.style.height = "7.5em";
         }
