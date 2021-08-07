6950c4393eb79f7579f126e86f574290b82fa8e8
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-07 10:09:44 +0100

    obj delete method

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,20 +1,29 @@
 export const projects = {
-    Default: [],
+  Default: [],
 };
 
 Object.defineProperty(Object.prototype, "addToProject", {
-    value: (project, objToAdd) => {
-        if (!projects[project]) {
-            projects[project] = [];
-        }
-        projects[project].push(objToAdd);
-    },
-    enumerable: false,
+  value: (project, objToAdd) => {
+    if (!projects[project]) {
+      projects[project] = [];
+    }
+    projects[project].push(objToAdd);
+  },
+  enumerable: false,
 });
 
 Object.defineProperty(Object.prototype, "returnProject", {
-    value: (project) => {
-        return projects[project];
-    },
-    enumerable: false,
+  value: (project) => {
+    return projects[project];
+  },
+  enumerable: false,
+});
+
+Object.defineProperty(Object.prototype, "removeFromProject", {
+  value: (project, titleOfObjToRemove) => {
+    projects[project] = projects[project].filter(() => {
+      return titleOfObjToRemove === projects[project][0];
+    });
+  },
+  enumerable: false,
 });
