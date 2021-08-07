ec5e7c0d10686419fcc88d8393352e5cf5ed9c9d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-07 11:49:24 +0100

    add remove project method

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,29 +1,36 @@
 export const projects = {
-  Default: [],
+    Default: [],
 };
 
 Object.defineProperty(Object.prototype, "addToProject", {
-  value: (project, objToAdd) => {
-    if (!projects[project]) {
-      projects[project] = [];
-    }
-    projects[project].push(objToAdd);
-  },
-  enumerable: false,
+    value: (project, objToAdd) => {
+        if (!projects[project]) {
+            projects[project] = [];
+        }
+        projects[project].push(objToAdd);
+    },
+    enumerable: false,
 });
 
 Object.defineProperty(Object.prototype, "returnProject", {
-  value: (project) => {
-    return projects[project];
-  },
-  enumerable: false,
+    value: (project) => {
+        return projects[project];
+    },
+    enumerable: false,
 });
 
 Object.defineProperty(Object.prototype, "removeFromProject", {
-  value: (project, titleOfObjToRemove) => {
+    value: (project, titleOfObjToRemove) => {
         projects[project] = projects[project].filter((obj) => {
-      return titleOfObjToRemove !== obj.title;
-    });
-  },
-  enumerable: false,
+            return titleOfObjToRemove !== obj.title;
+        });
+    },
+    enumerable: false,
+});
+
+Object.defineProperty(Object.prototype, "removeProject", {
+    value: (project) => {
+        delete projects[project];
+    },
+    enumerable: false,
 });
