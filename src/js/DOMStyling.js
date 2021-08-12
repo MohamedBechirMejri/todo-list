f121de2283c947dd3e77836a718f5a427a01a5a7
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-12 16:16:33 +0100

    feat: :lipstick: toggle nav

--- src/js/DOMStyling.js
+++ src/js/DOMStyling.js
@@ -1,21 +1,12 @@
 export const DOMStyling = (() => {
-  const navToggle = document.getElementById("nav-toggle");
+    const navToggle = document.getElementById("nav-toggle");
 
-  navToggle.addEventListener("click", () => {
-    /*    const gapDiv = document.getElementById("gap");
-            gapDiv.style.height !== "0em" ?
-                (gapDiv.style.height = "0em") :
-                (gapDiv.style.height = "6.5em");
-
-            const navListItems = document.querySelectorAll("li");
-            navListItems.forEach((item) => {
-                item.style.height !== "0em" ?
-                    (item.style.height = "0em") :
-                    (item.style.height = "2em");
-            }); */
-    const nav = document.querySelector("nav");
-    nav.style.marginTop !== "5em"
-      ? (nav.style.marginTop = "5em")
-      : (nav.style.marginTop = "-8em");
-  });
+    navToggle.addEventListener("click", () => {
+        const navListItems = document.querySelectorAll("li");
+        navListItems.forEach((item) => {
+            item.classList.toggle("hidden");
+        });
+        const nav = document.querySelector("nav");
+        nav.classList.toggle("hide-nav");
+    });
 })();
