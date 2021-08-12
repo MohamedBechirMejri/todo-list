6acb4c200a6c1a7445bd2fe6883cdc850cbafee5
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2021-08-12 15:25:42 +0100

    style: :art: beutify code

--- src/js/localStorage.js
+++ src/js/localStorage.js
@@ -1,25 +1,30 @@
 import localforage from "localforage";
-import { generateNotes, generateProjectsList } from "./DOMStuff";
-import { projects } from "./projects";
+import {
+    generateNotes,
+    generateProjectsList
+} from "./DOMStuff";
+import {
+    projects
+} from "./projects";
 
 export const checkLocalStorage = () => {
-  localforage.getItem("projects").then((v) => {
-    v ? loadFromLocalStorage() : saveToLocalStorage();
-  });
+    localforage.getItem("projects").then((v) => {
+        v ? loadFromLocalStorage() : saveToLocalStorage();
+    });
 };
 
 export const loadFromLocalStorage = () => {
-  localforage.getItem("projects").then((val) => {
-    Object.keys(val).forEach((project) => {
-      val[project].forEach((obj) => {
-        projects.addToProject(project, obj);
-      });
+    localforage.getItem("projects").then((val) => {
+        Object.keys(val).forEach((project) => {
+            val[project].forEach((obj) => {
+                projects.addToProject(project, obj);
+            });
+        });
+        generateProjectsList(projects);
+        generateNotes(projects[Object.keys(projects)[0]]);
     });
-    generateProjectsList(projects);
-    generateNotes(projects[Object.keys(projects)[0]]);
-  });
 };
 
 export const saveToLocalStorage = () => {
-  localforage.setItem("projects", projects);
+    localforage.setItem("projects", projects);
 };
